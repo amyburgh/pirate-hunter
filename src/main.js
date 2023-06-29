08717b09e91f4ae1f58919fbd2f4240cb296d3e1
amyburgh <air.myburgh@gmail.com>
2023-06-29 12:30:12 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>Pirate Hunters</div>
+`
+
+console.log('App running')
+// setupCounter(document.querySelector('#counter'))
